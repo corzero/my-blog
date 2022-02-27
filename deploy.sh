2084da3f97f9c468067351e6f5cbaa57cf671df7
corzero <cortejo@163.com>
2022-02-27 17:33:22 +0800

    chore: update md by script execution

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ echo "正在压缩...."
 
 zip -q -r public.zip ./public
 
+zip -q -r images.zip ./.vuepress/public/image
+
 echo "正在上传...."
 
 scp public.zip root@web:/usr/blog
@@ -15,6 +17,10 @@ scp public.zip root@web:/usr/blog
 rm -rf public public.zip
 
 ssh root@web "cd /usr/blog;rm -rf public;unzip public.zip; rm public.zip" 
+
+scp images.zip root@web:/usr/blog/public
+
+rm -rf images.zip
 
 echo "完成部署..."
 
